e1a8340e974a551bb2e92fbe11c21ed4d61fcc04
Omunkhuush <omunkhuush01@gmail.com>
2023-10-27 10:36:29 +0400

    update train_vit.py

--- train_vit.py
+++ train_vit.py
@@ -8,50 +8,57 @@ import torchvision
 import torchvision.transforms as transforms
 import wandb
 from pytorch_pretrained_vit import ViT
+
 import argparse
 torch.cuda.empty_cache()
 parser = argparse.ArgumentParser(description="Your script description")
-parser.add_argument("--resume", type=str, default=None, help="Path to checkpoint file for resuming training")
+parser.add_argument("--resume", type=str, default=None,
+                    help="Path to checkpoint file for resuming training")
 args = parser.parse_args()
 
-batch_size = 16
+batch_size = 32
 epochs = 500
 lr = 3e-5
 num_classes = 225
 
-wandb.init(project="AUTSL100_colorData_ViT", name='AUTSL_all_data_different_color_pretrained_384', config={
+wandb.init(project="AUTSL100_colorData_ViT", name='AUTSL_all_data_pretrained_384', config={
            "learning_rate": lr})
-#desired_image_size = (128, 128) 
+#desired_image_size = (128, 128)
 
 data_transforms = transforms.Compose([
-    transforms.Resize((384, 384)), 
+    transforms.Resize((384, 384)),
     transforms.ToTensor(),
-#    transforms.Lambda(lambda x: x.unsqueeze(0)), 
+    #    transforms.Lambda(lambda x: x.unsqueeze(0)),
     transforms.Normalize(0.5, 0.5),
 ])
 
 # Tensor Transforms (with Augmentation) and Pytorch Preprocessing:
-train_ds = torchvision.datasets.ImageFolder("../AUTSL_converted_data/train", transform=data_transforms)
-valid_ds = torchvision.datasets.ImageFolder("../AUTSL_converted_data/val", transform=data_transforms)
-test_ds = torchvision.datasets.ImageFolder("../AUTSL_converted_data/test", transform=data_transforms)
+train_ds = torchvision.datasets.ImageFolder(
+    "../AUTSL_converted_data/train", transform=data_transforms)
+valid_ds = torchvision.datasets.ImageFolder(
+    "../AUTSL_converted_data/val", transform=data_transforms)
+test_ds = torchvision.datasets.ImageFolder(
+    "../AUTSL_converted_data/test", transform=data_transforms)
 
 # Data Loaders:
 train_loader = data.DataLoader(train_ds, batch_size=batch_size, num_workers=4)
 valid_loader = data.DataLoader(valid_ds, batch_size=batch_size, num_workers=4)
-test_loader  = data.DataLoader(test_ds, batch_size=batch_size, num_workers=4)
+test_loader = data.DataLoader(test_ds, batch_size=batch_size, num_workers=4)
 
 # Training device:
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
 class CustomHead(nn.Module):
     def __init__(self, num_classes):
         super(CustomHead, self).__init__()
-        self.fc = nn.Linear(768, num_classes)  # Assuming the ViT model outputs 768 features
+        # Assuming the ViT model outputs 768 features
+        self.fc = nn.Linear(768, num_classes)
 
     def forward(self, x):
         x = self.fc(x)
         return x
-    
+
 
 epoch = 0
 if args.resume is not None:
@@ -68,7 +75,7 @@ if args.resume is not None:
     epoch = checkpoint['epoch']
     loss = checkpoint['loss']
 
-else: 
+else:
     print("not resuming")
     model = ViT('B_16_imagenet1k', pretrained=True)
     model.head = CustomHead(num_classes)
@@ -84,8 +91,8 @@ optimizer = optim.Adam(model.parameters(), lr=lr)
 
 best_val_loss = float('inf')
 
-for epoch in range(epoch,epochs):
-    print("number of epochs: ",epoch)
+for epoch in range(epoch, epochs):
+    print("number of epochs: ", epoch)
     model.train()
     running_loss = 0.0
     for data, label in tqdm(train_loader):
@@ -125,16 +132,28 @@ for epoch in range(epoch,epochs):
             best_val_loss = val_loss
             best_epoch = epoch
             bestModelPath = 'weights/best_all_data.pth'
-            torch.save({'epoch':epoch,
-                        'model_state_dict':model.module.state_dict(),
-                        'optimizer_state_dict':optimizer.state_dict(),
-                        'loss':val_loss},bestModelPath)
-        lastModelPath = 'weights/last_all_data.pth' 
-        torch.save({'epoch':epoch,
-                        'model_state_dict':model.module.state_dict(),
-                        'optimizer_state_dict':optimizer.state_dict(),
-                        'loss':val_loss},bestModelPath)
-                        
+            if torch.cuda.device_count() > 1:
+                torch.save({'epoch': epoch,
+                            'model_state_dict': model.module.state_dict(),
+                            'optimizer_state_dict': optimizer.state_dict(),
+                            'loss': val_loss}, bestModelPath)
+            else:
+                torch.save({'epoch': epoch,
+                            'model_state_dict': model.state_dict(),
+                            'optimizer_state_dict': optimizer.state_dict(),
+                            'loss': val_loss}, bestModelPath)
+
+        lastModelPath = 'weights/last_all_data.pth'
+        if torch.cuda.device_count() > 1:
+            torch.save({'epoch': epoch,
+                        'model_state_dict': model.module.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        'loss': val_loss}, lastModelPath)
+        else:
+            torch.save({'epoch': epoch,
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        'loss': val_loss}, lastModelPath)
         wandb.log({"Training Loss": train_loss,
                    "Validation Loss": val_loss,
                    "Validation Accuracy": (100 * correct / total)})
@@ -155,7 +174,7 @@ with torch.no_grad():
         labels = label.to(device)
         outputs = model(inputs)
         loss_t = criterion(outputs, labels)
-        test_loss+=loss_t.item()
+        test_loss += loss_t.item()
         _, predicted = torch.max(outputs, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
